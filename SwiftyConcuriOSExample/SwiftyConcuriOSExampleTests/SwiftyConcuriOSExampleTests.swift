c3e276dc6b7eeea3233a19d60e249f374c1996cf
brennick <brennick@users.noreply.github.com>
2015-07-15 16:53:21 -0700

    Setting up test pods and files

--- SwiftyConcuriOSExample/SwiftyConcuriOSExampleTests/SwiftyConcuriOSExampleTests.swift
+++ SwiftyConcuriOSExample/SwiftyConcuriOSExampleTests/SwiftyConcuriOSExampleTests.swift
@@ -1,36 +1,30 @@
-//
-//  SwiftyConcuriOSExampleTests.swift
-//  SwiftyConcuriOSExampleTests
-//
-//  Created by Brennick, Conner on 7/10/15.
-//  Copyright (c) 2015 Concur. All rights reserved.
-//
-
-import UIKit
 import XCTest
+import SwiftyConcur
 
 class SwiftyConcuriOSExampleTests: XCTestCase {
+  
+  var concurClient: ConcurClient!
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // Insert a Consumer Key and Consumer Secret here to test
+    var client = ConcurClient(consumerKey: "", consumerSecret: "")
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  func testExample() {
+      // This is an example of a functional test case.
+      XCTAssert(true, "Pass")
+  }
+  
+  func testCreateQuickExpense() {
+    XCTAssertNil(nil, "Pass")
+  }
     
 }
