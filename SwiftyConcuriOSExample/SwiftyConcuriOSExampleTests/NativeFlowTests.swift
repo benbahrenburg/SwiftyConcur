17dc34ed84ede18b05e5a7d115eed98dad74e2ee
brennick <brennick@users.noreply.github.com>
2015-07-16 09:50:44 -0700

    Native flow tests

--- SwiftyConcuriOSExample/SwiftyConcuriOSExampleTests/NativeFlowTests.swift
+++ SwiftyConcuriOSExample/SwiftyConcuriOSExampleTests/NativeFlowTests.swift
@@ -1,30 +1,25 @@
 import XCTest
 import SwiftyConcur
 
-class SwiftyConcuriOSExampleTests: XCTestCase {
+class NativeFlowTests: XCTestCase {
   
+  var environmentVariables = NSProcessInfo.processInfo().environment
   var concurClient: ConcurClient!
   
   override func setUp() {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
-    // Insert a Consumer Key and Consumer Secret here to test
-    var client = ConcurClient(consumerKey: "", consumerSecret: "")
+    // Insert Consumer Key and Consumer Secret here
   }
   
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
+  
+  func testGetAccessToken() {
     
-  func testExample() {
-      // This is an example of a functional test case.
-      XCTAssert(true, "Pass")
   }
   
-  func testCreateQuickExpense() {
-    XCTAssertNil(nil, "Pass")
-  }
-    
 }
