263a698436502e44bf59c264bc28d8f8e8a0536d
Conner Brennick <mail@cbrennick.com>
2015-08-11 08:36:33 -0700

    Digital Tax Invoices tests

---
+++ SwiftyConcuriOSExample/SwiftyConcuriOSExampleTests/DigitalTaxInvoicesTests.swift
@@ -0,0 +1,9 @@
+//
+//  DigitalTaxInvoicesTests.swift
+//  SwiftyConcuriOSExample
+//
+//  Created by Conner Brennick on 8/11/15.
+//  Copyright (c) 2015 Concur. All rights reserved.
+//
+
+import Foundation
