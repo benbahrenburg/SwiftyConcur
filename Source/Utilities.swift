2c80fd1c9ef453d6802d2f0bbd49155bcc0970c2
Conner Brennick <mail@cbrennick.com>
2015-08-11 16:36:04 -0700

    Fixes to make tests work

--- Source/Utilities.swift
+++ Source/Utilities.swift
@@ -39,6 +39,12 @@ public extension ConcurClient {
     return self.createRequest("DELETE", endpoint: endpoint, options: options)
   }
   
+  internal class func base64Encode(toEncode: String) -> String {
+    let utf8Encoded = toEncode.dataUsingEncoding(NSUTF8StringEncoding)
+    let base64Encoded = utf8Encoded?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros)
+    return base64Encoded!
+  }
+  
   internal class func sendRequest<T>(request: NSURLRequest, callback: (error: String!, returnValue: ConcurCollection<T>!) -> Void) {
     if self.authString != nil {
       Alamofire.request(request).responseJSON { (req, res, json, error) in
@@ -64,13 +70,7 @@ public extension ConcurClient {
     }
   }
   
-  internal class func base64Encode(toEncode: String) -> String {
-    let utf8Encoded = toEncode.dataUsingEncoding(NSUTF8StringEncoding)
-    let base64Encoded = utf8Encoded?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros)
-    return base64Encoded!
-  }
-  
-  private class func createRequest(method: String, endpoint: String, options: [String : AnyObject?]) -> NSMutableURLRequest! {
+  internal class func createRequest(method: String, endpoint: String, options: [String : AnyObject?]) -> NSMutableURLRequest! {
     // Adds endpoint to end of instance URL
     var urlString = self.instanceUrl.stringByAppendingString(endpoint)
     
@@ -97,9 +97,10 @@ public extension ConcurClient {
       request.HTTPMethod = method
       
       // Encodes the body dictionary into NSData
-      if let body = options["Body"] as? [String : String] {
+      if let body = options["Body"] as? [String : AnyObject] {
         var error: NSError?
         var bodyData = NSJSONSerialization.dataWithJSONObject(body, options: NSJSONWritingOptions.allZeros, error: &error)
+        NSURLProtocol.setProperty(bodyData!, forKey: "BodyData", inRequest: request)
         request.HTTPBody = bodyData
       }
       
