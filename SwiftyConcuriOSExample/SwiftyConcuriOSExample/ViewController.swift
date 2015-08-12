6115551a960e3864d6b577d89e0f0a95d4abaca1
Conner Brennick <mail@cbrennick.com>
2015-08-12 09:46:34 -0700

    Latest changes for demo, disconnect and connect working

--- SwiftyConcuriOSExample/SwiftyConcuriOSExample/ViewController.swift
+++ SwiftyConcuriOSExample/SwiftyConcuriOSExample/ViewController.swift
@@ -5,7 +5,14 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    if UIApplication.sharedApplication().canOpenURL(NSURL(string: "concurmobile://")!) == false {
+      self.connectButton.hidden = true
+    }
+    if let accessToken = NSUserDefaults.standardUserDefaults().objectForKey("ConcurAccessToken") as? String {
+      self.connectButton.setTitle("Disconnect With Concur", forState: UIControlState.Normal)
+    } else {
+      self.connectButton.setTitle("Connect With Concur", forState: UIControlState.Normal)
+    }
   }
   
   override func didReceiveMemoryWarning() {
@@ -14,13 +21,20 @@ class ViewController: UIViewController {
   }
   
   @IBAction func onTouch(sender: AnyObject) {
-    var client = ConcurClient(consumerKey: "", consumerSecret: "")
-    client.openWebFlow(self, scopeList: ["EXPRPT"], state: "weirdstate", callback: { (error, token, state) in
-      println(error)
-      println(token)
-      println(state)
-    })
+    if self.connectButton.titleLabel!.text == "Connect With Concur" {
+      var client = ConcurClient(consumerKey: "", consumerSecret: "")
+      client.openWebFlow(self, scopeList: ["EXPRPT"], state: "weirdstate", callback: { (error, token, state) in
+        if error == nil && token != nil {
+          NSUserDefaults.standardUserDefaults().setObject(token.Token, forKey: "ConcurAccessToken")
+          self.connectButton.setTitle("Disconnect With Concur", forState: UIControlState.Normal)
+        }
+      })
+    } else {
+      NSUserDefaults.standardUserDefaults().removeObjectForKey("ConcurAccessToken")
+      self.connectButton.setTitle("Connect With Concur", forState: UIControlState.Normal)
+    }
   }
   
+  @IBOutlet weak var connectButton: UIButton!
 }
 
